029e3c7cfab8a65086e094aa0a9dfb5eb3de53b9
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2026-02-11 14:06:24 +0330

    add Makefile to the project

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	go build -o CartopherCopilot
+
+run:
+	go run .
+
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/saleh-ghazimoradi/CartopherCopilot/cmd"
-
-func main() {
-	cmd.Execute()
-}
